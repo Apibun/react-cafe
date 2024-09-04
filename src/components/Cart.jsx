8a4e815c60ad071f1e9016795692fb3b0ec58dba
Apibun <BkApibun@gmail.com>
2024-09-05 02:59:16 +0700

    refactor code component cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -8,11 +8,86 @@ import { removeProduct } from '../reducers/cartSlice'
 import ConfirmOrderModal from './ConfirmOrderModal'
 import { resetCart } from '../reducers/cartSlice'
 
+const ProductInCart = ({ product }) => {
+  const dispatch = useDispatch()
+
+  const totalPrice = product.price * product.quantity
+
+  return (
+    <div className="flex justify-between items-center py-3" key={product.name}>
+      <div className="flex flex-col justify-center leading-7">
+        <div className="font-semibold text-primary-900">{product.name}</div>
+        <div className="flex gap-x-2">
+          <p className="mr-2 text-red font-semibold">
+            {`${product.quantity}x`}
+          </p>
+          <p className="text-primary-400">{`@ ${currencyFormatter.format(
+            product.price
+          )}`}</p>
+          <p className="text-primary-500 font-semibold">
+            {currencyFormatter.format(totalPrice)}
+          </p>
+        </div>
+      </div>
+      <button
+        onClick={() => dispatch(removeProduct(product))}
+        className="flex items-center justify-center w-5 h-5 border-2 border-primary-300 rounded-full"
+      >
+        <img width={12} src={removeIcon} alt="remove icon" />
+      </button>
+    </div>
+  )
+}
+
+const EmptyCart = () => {
+  return (
+    <div className="flex flex-col items-center py-5">
+      <div className="mt-1 mb-3">
+        <img src={emptyCartImg} alt="empty cart image" />
+      </div>
+      <p className="text-primary-500 font-semibold">
+        Your added items will appear here
+      </p>
+    </div>
+  )
+}
+
+const ProductInModal = ({ product }) => {
+  const totalPrice = product.price * product.quantity
+
+  return (
+    <div className="flex justify-between items-center py-4" key={product.name}>
+      <div className="flex gap-x-4">
+        <div className="w-12">
+          <img
+            className="object-contain rounded-md"
+            src={product.image.thumbnail}
+            alt="image thumbnail"
+          />
+        </div>
+        <div className="w-[9.5rem] lg:w-auto text-sm leading-6">
+          <div className="font-semibold truncate">{product.name}</div>
+          <div className="flex gap-x-4">
+            <p className="text-red font-semibold">{`${product.quantity}x`}</p>
+            <p className="text-primary-500">{`@ ${currencyFormatter.format(
+              product.price
+            )}`}</p>
+          </div>
+        </div>
+      </div>
+      <div className="text-sm font-semibold">
+        {currencyFormatter.format(totalPrice)}
+      </div>
+    </div>
+  )
+}
+
 const Cart = () => {
   const [showModal, setShowModal] = useState(false)
   const cart = useSelector((state) => state.cart.items)
   const dispatch = useDispatch()
 
+  const isNotEmptyCart = cart.length > 0
   const orderAmount = cart.reduce((acc, value) => acc + value.quantity, 0)
   const sumPrice = cart.reduce(
     (acc, value) => acc + value.quantity * value.price,
@@ -29,40 +104,11 @@ const Cart = () => {
       <div className="mb-3 text-red text-2xl font-bold">
         Your Cart {`(${orderAmount})`}
       </div>
-      {cart.length > 0 ? (
+      {isNotEmptyCart ? (
         <div className="divide-y divide-gray-100">
-          {cart.map((product) => {
-            const totalPrice = product.price * product.quantity
-            return (
-              <div
-                className="flex justify-between items-center py-3"
-                key={product.name}
-              >
-                <div className="flex flex-col justify-center leading-7">
-                  <div className="font-semibold text-primary-900">
-                    {product.name}
-                  </div>
-                  <div className="flex gap-x-2">
-                    <p className="mr-2 text-red font-semibold">
-                      {`${product.quantity}x`}
-                    </p>
-                    <p className="text-primary-400">{`@ ${currencyFormatter.format(
-                      product.price
-                    )}`}</p>
-                    <p className="text-primary-500 font-semibold">
-                      {currencyFormatter.format(totalPrice)}
-                    </p>
-                  </div>
-                </div>
-                <button
-                  onClick={() => dispatch(removeProduct(product))}
-                  className="flex items-center justify-center w-5 h-5 border-2 border-primary-300 rounded-full"
-                >
-                  <img width={12} src={removeIcon} alt="remove icon" />
-                </button>
-              </div>
-            )
-          })}
+          {cart.map((product) => (
+            <ProductInCart key={product.name} product={product} />
+          ))}
           <div className="flex justify-between items-center py-6">
             <p className="text-primary-900 text-sm">Order Total</p>
             <div className="text-xl font-bold">
@@ -84,44 +130,12 @@ const Cart = () => {
           </button>
         </div>
       ) : (
-        <div className="flex flex-col items-center py-5">
-          <div className="mt-1 mb-3">
-            <img src={emptyCartImg} alt="empty cart image" />
-          </div>
-          <p className="text-primary-500 font-semibold">
-            Your added items will appear here
-          </p>
-        </div>
+        <EmptyCart />
       )}
       <ConfirmOrderModal isShowModal={showModal}>
         <div className="px-5 pt-2 my-7 rounded-lg bg-primary-50 divide-y">
           {cart.map((product) => (
-            <div
-              className="flex justify-between items-center py-4"
-              key={product.name}
-            >
-              <div className="flex gap-x-4">
-                <div className="w-12">
-                  <img
-                    className="object-contain rounded-md"
-                    src={product.image.thumbnail}
-                    alt="image thumbnail"
-                  />
-                </div>
-                <div className="w-[9.5rem] lg:w-auto text-sm leading-6">
-                  <div className="font-semibold truncate">{product.name}</div>
-                  <div className="flex gap-x-4">
-                    <p className="text-red font-semibold">{`${product.quantity}x`}</p>
-                    <p className="text-primary-500">{`@ ${currencyFormatter.format(
-                      product.price
-                    )}`}</p>
-                  </div>
-                </div>
-              </div>
-              <div className="text-sm font-semibold">
-                {currencyFormatter.format(product.price * product.quantity)}
-              </div>
-            </div>
+            <ProductInModal product={product} />
           ))}
           <div className="flex justify-between items-center py-6">
             <p className="text-sm text-primary-900">Order Total</p>
